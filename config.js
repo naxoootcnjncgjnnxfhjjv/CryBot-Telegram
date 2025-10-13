5a83dd6a7c8d5a521a5b46b84dac72748bc1a10d
naxoootcnjncgjnnxfhjjv <182387950+naxoootcnjncgjnnxfhjjv@users.noreply.github.com>
2025-10-13 03:02:14 +0200

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
-// config.js
-// Configuración mínima para CryBot-Telegram
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export function loadConfig() {
   return {
-    rpcUrl: process.env.RPC_URL || "https://ethereum.publicnode.com",
-    privateKey: process.env.PRIVATE_KEY_EVM,
     botToken: process.env.BOT_TOKEN,
-    adminId: process.env.ADMIN_TELEGRAM_ID,
+    rpcUrl: process.env.RPC_URL || "https://eth.llamarpc.com",
+    privateKey: process.env.PRIVATE_KEY,
+    etherscanApiKey: process.env.ETHERSCAN_API_KEY,
+    tonApiKey: process.env.TON_API_KEY,
+    port: process.env.PORT || 3000,
   };
 }
