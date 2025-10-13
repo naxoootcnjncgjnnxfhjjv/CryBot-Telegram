854453ef04c19905609a2f97dce8d5f34a63c0a9
naxoootcnjncgjnnxfhjjv <182387950+naxoootcnjncgjnnxfhjjv@users.noreply.github.com>
2025-10-13 02:22:23 +0200

    Crear config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+// Archivo placeholder para CryBot-Telegram en Railway
+export function loadConfig() {
+  return {
+    rpcUrl: process.env.RPC_URL,
+    privateKey: process.env.PRIVATE_KEY_EVM
+  };
+}
