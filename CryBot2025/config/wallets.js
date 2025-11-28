4e195f6f0948505c4a8189ead7fecb0bfacfd2c1
naxoootcnjncgjnnxfhjjv <182387950+naxoootcnjncgjnnxfhjjv@users.noreply.github.com>
2025-11-28 07:54:52 +0100

    Add CryBot2025 config/wallets.js with wallet addresses

---
+++ CryBot2025/config/wallets.js
@@ -0,0 +1,28 @@
+export const wallets = {
+  main: "0x82219fc3B1d22f0DAd2703101724dfA8f08DC456",
+
+  evm: [
+    "0x7B9Fc90C99b2ae4711BDEe31049c357999e79B09",
+    "0x5e66fa97ebec4a39166cf5c323cdc6a3538f1848",
+    "0xf37465e2978d90a8feae048d0e15c338d04aa4d6",
+    "0x14287D44a3aA5D7025D2cAeBD415a2673F7bEC3E"
+  ],
+
+  ton: [
+    "TYKiqDj3XLEJs3wrMvnQshN19R524XfC7Q",
+    "UQChtGxrxo1H74kGde0GNsSKWYG_rhGMKNco-opmWQ1B-yil",
+    "UQDY-o0QuHWumsIKstom7sXBzlX2fQ27-cz4r01e9QatvWZU",
+    "UQAMPbQpQJtnPlS5aQQUYMv6uKeEQQg0YQ8bjn0IB1OgheTk",
+    "UQCcLv7JUPrlFZ7-504vbxfTxK6o93nHTwwN-Qv16NsCRy20",
+    "UQBSINhOenZdPyDmV3bfeQ1Hu-Z-zyITBJj0uisC0RmH0GxT"
+  ],
+
+  aptos: [
+    "0x11353909627b83813dee8d578a636bd042223308acebda7ff1e4220b861de6eb",
+    "0xc13873d72475e43d7b33cafa22b8f8123a64315ee999cf41ad1555f26aed5a3a"
+  ],
+
+  solana: [
+    "EEb1yJtp3X2nwf4ZoXHedGSna28q2CWWi3EJUAbcuTE7"
+  ]
+};
